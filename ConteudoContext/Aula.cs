b58bb6b926fa655a814579df25c854a1babd9a0e
Denys Rógeres <dennysrogeres@gmail.com>
2022-04-08 20:49:42 -0300

    Criando classes aula e modulo

---
+++ ConteudoContext/Aula.cs
@@ -0,0 +1,8 @@
+namespace Balta.ConteudoContext
+{
+    public class Aula
+    {
+        public int Ordem { get; set; }
+        public string? Titulo { get; set; }
+    }
+}
--- ConteudoContext/Curso.cs
+++ ConteudoContext/Curso.cs
@@ -9,21 +9,4 @@ namespace Balta.ConteudoContext
         public string? Tag { get; set; }
         public IList<Modulo> Modulos { get; set; }
     }
-
-    public class Modulo 
-    {
-        public Modulo()
-        {
-            Aulas = new List<Aula>();
-        }
-        public int Ordem { get; set; }
-        public string? Titulo { get; set; }
-        public IList<Aula> Aulas { get; set; }
-    }
-
-    public class Aula
-    {
-        public int Ordem { get; set; }
-        public string? Titulo { get; set; }
-    }
 }
